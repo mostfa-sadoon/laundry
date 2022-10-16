cc71ae962ccd26c4dfe6f3108200c7c31657a65e
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-10-16 10:38:38 +0200

    traits

--- app/Models/Laundry/Launbry.php
+++ app/Models/Laundry/Launbry.php
@@ -4,8 +4,12 @@ namespace App\Models\Laundry;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Launbry extends Model
+
+
+class Launbry extends Authenticatable
 {
     use HasFactory;
+    protected $guarded=[];
 }
--- app/Models/Laundry/Pranch.php
+++ app/Models/Laundry/Pranch.php
@@ -4,8 +4,11 @@ namespace App\Models\Laundry;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Pranch extends Model
+
+class Pranch extends Authenticatable
 {
     use HasFactory;
+    protected $guarded=[];
 }
