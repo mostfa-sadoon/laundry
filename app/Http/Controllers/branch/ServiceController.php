1dd9281e93a293c9e92a4278e8549142aa0135cf
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-11-14 00:51:15 +0200

    edit

--- app/Http/Controllers/branch/ServiceController.php
+++ app/Http/Controllers/branch/ServiceController.php
@@ -245,7 +245,9 @@ class ServiceController extends Controller
         foreach($branchservices as $branchservice){
             array_push($this->service_ids,$branchservice->service_id);
         }
-        $services=Service::select('id')->with(['categories'])->wherein('id',$this->service_ids)->select('id')->get()->makehidden(['created_at','updated_at']);
+        $services=Service::select('id')->with(['categories'=>function($q){
+            $q->whereHas('branchitems');
+        }])->wherein('id',$this->service_ids)->select('id')->get()->makehidden(['created_at','updated_at']);
         $branchservices=Serviceitemprice::select('additionalservice_id')->where('branch_id',$branch_id)->where('additionalservice_id','!=',null)->distinct()->get();
         foreach($branchservices as $branchservice){
             array_push($this->additionalservice_ids,$branchservice->additionalservice_id);
