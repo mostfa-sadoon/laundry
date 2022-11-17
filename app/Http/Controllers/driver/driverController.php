0c6ce5460954b3a47f2297b0e395759fc53a4f9a
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-11-17 14:02:49 +0200

    update_phone

--- app/Http/Controllers/driver/driverController.php
+++ app/Http/Controllers/driver/driverController.php
@@ -36,9 +36,9 @@ class driverController extends Controller
     }
     public function updateinfo(Request $request){
         $driver_id=Auth::guard('driver_api')->user()->id;
-        $driver=Driver::select('name','email','phone')->find($driver_id);
+        $driver=Driver::find($driver_id);
         $validator =Validator::make($request->all(),[
-            'name'=>'required|unique:drivers',
+            'name'=>'required|unique:drivers,name,'.$driver_id,
             'email'=>'required|unique:drivers,email,'.$driver_id,
             'phone'=>'required|unique:drivers,phone,'.$driver_id,
           ]);
@@ -47,25 +47,28 @@ class driverController extends Controller
                'message'=>$validator->messages()->first()
            ],403);
            }
+           if($request->phone==$driver->phone){
            $driver->update([
             'name'=>$request->name,
             'email'=>$request->email
-        ]);
+            ]);
             $data['status']=true;
             $data['message']="profile info updated successfully";
             return response()->json($data);
-            if($request->phone!=$driver->phone){
+            }else{
             $driver->update([
                 'otp'=>1234,
               ]);
               $data['status']=true;
               $data['message']="please send otp in the next request with phone number";
               return response()->json($data);
-        }
+           }
     }
 
     public function updatephone(Request $request){
-        $driver=Driver::where('otp',$request->otp)->first();
+        $driver=Driver::where('otp',$request->otp)->where('phone',$request->old_phone)->first();
+
+        // check of driver
         if($driver==null){
          $data['status']=false;
          $data['message']="some thing is wrong";
@@ -74,11 +77,20 @@ class driverController extends Controller
         if (!$token = auth()->guard('driver_api')->tokenById($driver->id)) {
          return response()->json(['message' => 'token is false'], 401);
         }
+        // check of phone
+        $validator =Validator::make($request->all(),[
+            'new_phone'=>'required|unique:drivers,phone,'.$driver->id,
+          ]);
+          if ($validator->fails()) {
+           return response()->json([
+               'message'=>$validator->messages()->first()
+           ],403);
+           }
          $driver->update([
              'otp'=>null
          ]);
         $driver->update([
-           'phone'=>$request->phone
+           'phone'=>$request->new_phone
         ]);
         $data['status']=true;
         $data['message']="profile info updated successfully";
