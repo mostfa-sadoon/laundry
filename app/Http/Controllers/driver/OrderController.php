68fcb5cc94019a9e8e158ad736725447808fe594
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-11-23 16:57:12 +0200

    inprogressinfo

--- app/Http/Controllers/driver/OrderController.php
+++ app/Http/Controllers/driver/OrderController.php
@@ -277,20 +277,10 @@ class OrderController extends Controller
         ->groupBy('orders.delivery_status')->groupBy('orders.created_at')
         ->groupBy('order_detailes.order_id')
         ->get();
-
         foreach($latestorders as $latestorder){
             $latestorder->created_at=date('Y-m-d', strtotime($latestorder->created_at));
             $latestorder->time=date('h:m a', strtotime($latestorder->created_at));
         }
-        // $completedorder=DB::table('orders')
-        // ->select('orders.id','orders.customer_name','orders.customer_phone','orders.customer_location')
-        // ->selectRaw('sum(order_detailes.quantity) as quantity')
-        // ->where('orders.driver_id',$driver_id)
-        // ->where('progress','=','completed')
-        // ->join('order_detailes','order_detailes.order_id','=','orders.id')
-        // ->groupBy('orders.id')->groupBy('orders.customer_name')->groupBy('orders.customer_phone')->groupBy('orders.customer_location')
-        // ->groupBy('order_detailes.order_id')
-        // ->get();
         $data['status']=true;
         $data['message']="get new orders suceesfully";
         $data['data']['orders']=$latestorders;
@@ -298,15 +288,118 @@ class OrderController extends Controller
         return response()->json($data);
      }
      public function latestorderinfo(Request $request){
-        dd($request->all());
+           $order_id=$request->order_id;
         $driver_id=Auth::guard('driver_api')->user()->id;
-        $order_id=$request->order_id;
-        DB::table('orders')
-        ->join('order_detailes','order_detailes.order_id','=','order.id')
+        $lang=$request->header('lang');
+        App::setLocale($lang);
+        $order=DB::table('order_detailes')->where('order_detailes.order_id',$order_id)
+        ->select('orders.delivery_status')
+        ->join('orders','orders.id','=','order_detailes.order_id')
         ->join('order_delivery_status','order_delivery_status.order_id','=','orders.id')
+        ->where('order_delivery_status.driver_id',$driver_id)->latest('order_delivery_status.id')
         ->where('order_delivery_status.confirmation',false)
-        ->where('orders.id',$order_id)
+        ->selectRaw('orders.id')
+        ->selectRaw('order_delivery_status.order_status')
+        ->selectRaw('sum(order_detailes.price) as price')
         ->groupBy('orders.id')
+        ->groupBy('orders.delivery_status')
+        ->groupBy('order_delivery_status.order_id')
+        ->groupBy('order_delivery_status.order_status')
+        ->first();
+        $orderargentprice=DB::table('order_detailes')->where('order_detailes.order_id',$order_id)
+        ->join('orders','orders.id','=','order_detailes.order_id')
+        ->join('argent','orders.id','=','argent.order_id')
+        ->selectRaw('sum(argent.price) as argentprice')
+         ->groupBy('argent.id')
+         ->first();
+         $order->price=$order->price+$orderargentprice->argentprice;
+        // this query get services with count of item in it
+        $services=DB::table('order_detailes')->where('order_detailes.order_id',$order_id)
+        ->join('servicetranslations','servicetranslations.service_id','=','order_detailes.service_id')
+        ->join('orders','orders.id','=','order_detailes.order_id')
+        ->join('branchitems','branchitems.id','=','order_detailes.branchitem_id')
+        ->join('branchitemtranslations','branchitemtranslations.branchitem_id','=','branchitems.id')
+        ->selectRaw('sum(quantity) as quantity')
+        ->selectRaw('servicetranslations.name as service')
+        ->selectRaw('servicetranslations.service_id as service_id')
+        ->where('order_detailes.order_id',$order_id)
+        ->where('servicetranslations.locale',$lang)
+        ->where('branchitemtranslations.locale',$lang)
+        ->where('order_detailes.additionalservice_id','=',null)
+        ->groupBy('servicetranslations.service_id')
+        ->groupBy('servicetranslations.name')
+        ->where('orders.driver_id',$driver_id)->where('order_detailes.order_id',$order_id)
         ->get();
+         // this query get items with count of item in it
+        $items=DB::table('order_detailes')->where('order_detailes.order_id',$order_id)
+        ->join('servicetranslations','servicetranslations.service_id','=','order_detailes.service_id')
+        ->join('orders','orders.id','=','order_detailes.order_id')
+        ->join('branchitems','branchitems.id','=','order_detailes.branchitem_id')
+        ->join('branchitemtranslations','branchitemtranslations.branchitem_id','=','branchitems.id')
+        ->selectRaw('branchitemtranslations.name')
+        ->selectRaw('sum(quantity) as quantity')
+        ->selectRaw('branchitemtranslations.branchitem_id as item_id')
+        ->selectRaw('servicetranslations.service_id')
+        ->where('order_detailes.order_id',$order_id)
+        ->where('servicetranslations.locale',$lang)
+        ->where('branchitemtranslations.locale',$lang)
+        ->where('order_detailes.additionalservice_id','=',null)
+        ->groupBy('servicetranslations.service_id')
+        ->groupBy('servicetranslations.name')
+        ->groupBy('branchitemtranslations.name')
+        ->groupBy('branchitemtranslations.branchitem_id')
+        ->where('orders.driver_id',$driver_id)->where('order_detailes.order_id',$order_id)
+        ->get();
+         // this query to get additional service
+        $additionals=DB::table('order_detailes')->where('order_detailes.order_id',$order_id)
+        ->join('orders','orders.id','=','order_detailes.order_id')
+        ->join('additionalservicetranslations','additionalservicetranslations.additionalservice_id','=','order_detailes.additionalservice_id')
+        ->join('branchitemtranslations','branchitemtranslations.branchitem_id','=','order_detailes.branchitem_id')
+        ->selectRaw('order_detailes.service_id')
+        ->selectRaw('sum(quantity) as quantity')
+        ->selectRaw('branchitemtranslations.name as item')
+        ->selectRaw('branchitemtranslations.branchitem_id as item_id')
+        ->selectRaw('additionalservicetranslations.name')
+        ->where('orders.driver_id',$driver_id)->where('order_detailes.order_id',$order_id)
+        ->where('order_detailes.additionalservice_id','!=',null)
+        ->groupBy('additionalservicetranslations.additionalservice_id')
+        ->groupBy('additionalservicetranslations.name')
+        ->groupBy('branchitemtranslations.name')
+        ->groupBy('branchitemtranslations.branchitem_id')
+        ->groupBy('order_detailes.service_id')
+        ->where('additionalservicetranslations.locale',$lang)
+        ->where('branchitemtranslations.locale',$lang)
+        ->get();
+          $argents=db::table('argent')->where('order_id',$order_id)->get();
+            // but additional service in the item
+           foreach($items as $key=>$item){
+            $item->additonalservice=[];
+            foreach($additionals as $additional){
+                if($item->item_id==$additional->item_id){
+                    array_push($item->additonalservice,$additional);
+                }
+             }
+             //but argent inside item
+             foreach($argents as $argent){
+                $item->argent=0;
+                if($item->item_id==$argent->branchitem_id){
+                    $item->argent=$argent->quantity;
+                }
+             }
+           }
+            //but argent inside item
+          foreach($services as $key=>$service){
+            $service->item=[];
+             foreach($items as $item){
+                if($item->service_id==$service->service_id){
+                    array_push($service->item,$item);
+                }
+             }
+          }
+        $data['status']=true;
+        $data['message']="get new orders suceesfully";
+        $data['data']['order']=$order;
+        $data['data']['serives']=$services;
+        return response()->json($data);
      }
 }
