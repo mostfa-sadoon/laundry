b93d39dbf3076e6fb5c7014796ff4a0cfafccd96
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-12-24 17:12:06 +0200

    dd

--- app/Http/Controllers/branch/NotificationController.php
+++ app/Http/Controllers/branch/NotificationController.php
@@ -24,7 +24,8 @@ class NotificationController extends Controller
         ->select('branchnotifytype.id as id','notificationtypetranslations.name','branchnotifytype.status')
         ->where('locale',$lang)
         ->where('branch_id',$branch_id)
-        ->get()->distinct();
+        ->distinct()
+        ->get();
         $data['data']['notifications']=$notifications;
         return $this->response(true,'get avilable driver successfully',$data);
     }
