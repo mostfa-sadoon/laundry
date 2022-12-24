2fb1d1fc3361085c1e673f178b7a6f8a5a8ffaac
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-12-24 17:18:48 +0200

    dd

--- app/Http/Controllers/branch/NotificationController.php
+++ app/Http/Controllers/branch/NotificationController.php
@@ -21,10 +21,12 @@ class NotificationController extends Controller
         $notifications=DB::table('notificationtypes')
         ->join('notificationtypetranslations','notificationtypetranslations.notificationtype_id','=','notificationtypes.id')
         ->join('branchnotifytype','branchnotifytype.notificationtype_id','=','notificationtypes.id')
-      //  ->select('branchnotifytype.id as id','notificationtypetranslations.name','branchnotifytype.status')
+        ->select('branchnotifytype.id as id','notificationtypetranslations.name','branchnotifytype.status')
         ->where('locale',$lang)
         ->where('branch_id',$branch_id)
         ->groupBy('notificationtypetranslations.name')
+        ->groupBy('branchnotifytype.id')
+       
         ->get();
         $data['data']['notifications']=$notifications;
         return $this->response(true,'get avilable driver successfully',$data);
